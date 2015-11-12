884df746410cc3a023873666602def5ad4d1bf3a
joshuaebowling <joshuaebowling@mail.com>
2015-11-12 17:02:01 -0600

    now loggin chunks on server side

--- public/javascripts/myio-stream.js
+++ public/javascripts/myio-stream.js
@@ -3,13 +3,16 @@
 
 	$file = $('#file');
 	$file.change(function(e) {
-		var file, stream;
+		var blobStream, file, stream;
 
 		file = e.target.files[0];
 		stream = ss.createStream();
 		console.log(file, stream);
 		ss(socket).emit('import-data', stream, {size: file.size, name: file.name});
-		ss.createBlobReadStream(file).pipe(stream);
+		blobStream = ss.createBlobReadStream(file, {highWaterMark:1024}).pipe(stream);
+		blobStream.on('data', function(chunk) {
+			console.log(chunk.length);
+		});
 	});
 	socket = io.connect('http://joshuaebowling.info:8003/upload');
 
