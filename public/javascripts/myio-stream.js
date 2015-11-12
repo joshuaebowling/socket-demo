cddf8264ec936334c97acf7dc9df62395921fbbc
joshuaebowling <joshuaebowling@mail.com>
2015-11-12 16:30:52 -0600

    initial upload functionality

--- public/javascripts/myio-stream.js
+++ public/javascripts/myio-stream.js
@@ -1,9 +1,16 @@
 (function($) {
 	var socket, $file;
 
-	socket = io.connect('http://joshuaebowling.info:8003/');
-	socket.on('news', function(data) {
-		console.log(data);
+	$file = $('#file');
+	$file.change(function(e) {
+		var file, stream;
+
+		file = e.target.files[0];
+		stream = ss.createStream();
+		console.log(file, stream);
+		ss(socket).emit('import-data', stream, {size: file.size, name: file.name});
+		ss.createBlobReadStream(file).pipe(stream);
 	});
+	socket = io.connect('http://joshuaebowling.info:8003/upload');
 
 })(jQuery);
