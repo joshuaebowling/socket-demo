ff5ef2ceff8ac4c0cb6b7d664db8e6c85f501eae
joshuaebowling <joshuaebowling@mail.com>
2015-11-13 09:18:15 -0600

    upload-response loop complete

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ change the ports in `ss-config.json`
 
 ## Fire it Up
 `npm start`
+
+## Caveats
+If you're going to run on localhost, you may have to do some configuration since the ports have to be different for each server. I built using my vps so I was able to bypass this issue.
--- public/javascripts/myio-stream.js
+++ public/javascripts/myio-stream.js
@@ -1,17 +1,25 @@
 (function($) {
-	var socket, $file;
-
+	var $file, $output, renderOutput, socket, template;
+	
 	$file = $('#file');
+	$output = $('#output');
+
+	template = _.template('<p><%= line %></p>');
+	renderOutput = function(output) {
+		$output.append(template({line:output}));
+	};
 	$file.change(function(e) {
 		var blobStream, file, stream;
 
 		file = e.target.files[0];
 		stream = ss.createStream();
-		console.log(file, stream);
 		ss(socket).emit('import-data', stream, {size: file.size, name: file.name});
 		blobStream = ss.createBlobReadStream(file, {highWaterMark:1024}).pipe(stream);
 		blobStream.on('data', function(chunk) {
 			console.log(chunk.length);
+		});
+		socket.on('column-data', function(data) {
+			renderOutput(data.data);
 		});
 	});
 	socket = io.connect('http://joshuaebowling.info:8003/upload');
